0ebf997e20cb1532abe0115edc66b933e7a7f579
https://github.com/MudehDieudonne <mukummudeh@gmail.com>
2025-04-29 20:37:45 +0100

    timeslotroutes

---
+++ src/routes/timeSlotRoutes.js
@@ -0,0 +1,14 @@
+import express from "express"
+import { authenticate, authorize } from "../middleware/auth.js"
+import { createTimeSlot, getProviderTimeSlots, getAvailableTimeSlots } from "../controllers/timeSlotController.js"
+
+const router = express.Router()
+
+//Provider ROUTES
+router.post('.provider/time-slots', authenticate, authorize(['provider']), createTimeSlot)
+router.get('/provider/time-slots', authenticate, authorize(['provider']), getProviderTimeSlots)
+
+//CLient route
+router.get('/time-slots/available', authenticate, getAvailableTimeSlots)
+
+export default router
