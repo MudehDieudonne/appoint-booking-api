e41ed61777fe642e9ee5582209f49022e426d191
https://github.com/MudehDieudonne <mukummudeh@gmail.com>
2025-10-20 08:53:27 +0100

    befor branch switch

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,7 +2,7 @@ import express from "express"
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.send('Welcome to express 101')
+    res.send('Welcome to express Appointment booking sytem')
 })
 
 export default router
